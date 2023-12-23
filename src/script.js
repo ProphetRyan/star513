e0db2543ab41579fbb99cb938e9a9670f28247ce
Dan Ryan <iamanimapro@gmail.com>
2023-12-23 17:51:16 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -65,7 +65,7 @@ controls.maxPolarAngle = Math.PI*0.6
 //Textures
 const textureLoader = new THREE.TextureLoader();
 
-const env_texture = textureLoader.load("/textures/v2.png");
+const env_texture = textureLoader.load("/textures/star env.v2.png");
 
 env_texture.mapping = THREE.EquirectangularReflectionMapping;
 env_texture.flipY = false;
